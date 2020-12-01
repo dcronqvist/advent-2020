ca573b6c856d4d4afd202e05ff3969990c8321f3
Daniel Cronqvist <dancro99@gmail.com>
2020-12-01 06:33:22 +0100

    Day 1 complete

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # advent-2020
 ❄️ Repo for my solutions to Advent of Code 2020
+
+- **Day 1**: Interesting first day! Site went down, so was unable to retrieve my input. Site was back up in about 3 minutes, and I had already written my code, so it was just a matter of copy pasting. Got place 370 on first part and 167 on part two, globally.
--- day-1/solution.py
+++ day-1/solution.py
@@ -2,10 +2,32 @@ def read_input():
     with open("input.txt", "r") as f:
         return f.read()
 
-def read_input_lines():
+def read_input_lines(cast):
     with open("input.txt", "r") as f:
-        return [l.strip() for l in f.readlines()]
+        return [cast(l.strip()) for l in f.readlines()]
 
 def read_input_separated(c):
     with open("input.txt", "r") as f:
         return f.read().split(c)
+
+lines = read_input_lines(int)
+
+# PART 1
+def part1():
+    for num1 in lines:
+        for num2 in lines:
+            if num1 + num2 == 2020:
+                return num1 * num2
+
+# PART 2
+# We only have input of N = 200, N^3 will be 8 * 10^6, will run in a few seconds.
+# literal definition spaghetti code <3, but it gets the job done.
+def part2():
+    for num1 in lines:
+        for num2 in lines:
+            for num3 in lines:
+                if num1 + num2 + num3 == 2020:
+                    return num1 * num2 * num3
+
+print(f"Part 1: {part1()}")
+print(f"Part 2: {part2()}")
