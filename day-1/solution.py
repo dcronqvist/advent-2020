55f433f020c954fca32cb935b7b9825938da278b
Daniel Cronqvist <dancro99@gmail.com>
2020-12-01 08:47:56 +0100

    Removed input from day-1 and improved complexity

--- day-1/solution.py
+++ day-1/solution.py
@@ -10,24 +10,42 @@ def read_input_separated(c):
     with open("input.txt", "r") as f:
         return f.read().split(c)
 
+def binary_search(li, item):
+    li.sort()
+    upper = len(li) - 1
+    lower = 0
+    middle = int((upper + lower) / 2)
+    while li[middle] != item:
+        if li[middle] > item:
+            upper = middle
+        elif li[middle] < item:
+            lower = middle
+        middle = int((upper + lower) / 2)
+        if upper <= lower + 1:
+            break
+        if li[middle] == item:
+            return True
+    return False
+
 lines = read_input_lines(int)
 
 # PART 1
-def part1():
+def part1(lines):
     for num1 in lines:
-        for num2 in lines:
-            if num1 + num2 == 2020:
-                return num1 * num2
+        remainder = 2020 - num1
+        if binary_search(lines, remainder):
+            return num1 * remainder
 
 # PART 2
 # We only have input of N = 200, N^3 will be 8 * 10^6, will run in a few seconds.
 # literal definition spaghetti code <3, but it gets the job done.
 def part2():
     for num1 in lines:
+        remainder = 2020 - num1
         for num2 in lines:
-            for num3 in lines:
-                if num1 + num2 + num3 == 2020:
-                    return num1 * num2 * num3
+            if binary_search(lines, remainder - num2):
+                    return num1 * num2 * (remainder - num2)
 
-print(f"Part 1: {part1()}")
+print(f"Part 1: {part1(lines)}")
 print(f"Part 2: {part2()}")
+    
